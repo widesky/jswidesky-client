a495120a667c30430d828655927e9d618ee40b70
Stuart Longland <stuartl@vrt.com.au>
2018-10-30 09:51:20 +1000

    WSHUB-100: Add entrypoint.

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ * vim: set tw=78 et ts=4 sw=4 si fileencoding=utf-8:
+ *
+ * WideSky API Client entrypoint.
+ * (C) 2018 VRT Systems
+ */
+"use strict";
+
+var client = require('./src/client'),
+    data = require('./src/data');
+
+/* Exported symbols */
+module.exports = {
+    /* Client code */
+    WideSkyClient: client,
+    /* Data types */
+    Ref: data.Ref,
+    String: data.String,    // polyfilled
+    Number: data.Number,    // polyfilled
+    Date: data.Date,        // polyfilled
+    HSNumber: data.HSNumber
+};
