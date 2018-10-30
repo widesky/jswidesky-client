272179f91f4691cce40985cff497d94e301a10a3
Stuart Longland <stuartl@vrt.com.au>
2018-10-30 15:19:28 +1000

    WSHUB-100: test stubs: Create some helper functions.

--- test/stubs.js
+++ test/stubs.js
@@ -6,7 +6,9 @@
  */
 "use strict";
 
-const sinon = require('sinon');
+const sinon = require('sinon'),
+    expect = require('chai').expect,
+    WideSkyClient = require('../src/client');
 
 /**
  * StubLogger is a logger object that spoofs a Bunyan-style logger.
@@ -66,9 +68,82 @@ const StubHTTPClient = function() {
     };
 };
 
+const WS_URI = 'http://localhost:3000',
+    WS_USER = 'user@example.com',
+    WS_PASSWORD = 'password',
+    WS_CLIENT_ID = 'client id string',
+    WS_CLIENT_SECRET = 'client secret',
+    WS_ACCESS_TOKEN = 'an access token',
+    WS_REFRESH_TOKEN = 'a refresh token',
+    WS_ACCESS_TOKEN2 = 'another access token',
+    WS_REFRESH_TOKEN2 = 'another refresh token';
+
+const getInstance = function(http, log) {
+    let c = new WideSkyClient(
+        WS_URI, WS_USER, WS_PASSWORD, WS_CLIENT_ID, WS_CLIENT_SECRET, log
+    );
+    http.stubClient(c);
+    return c;
+};
+
+const authHandler = function(opts) {
+    var body;
+    opts = opts || {};
+
+    if (opts.refresh) {
+        body = {
+            refresh_token: opts.expect_refresh_token || WS_REFRESH_TOKEN,
+            grant_type: 'refresh_token'
+        };
+    } else {
+        body = {
+            username: opts.expect_user || WS_USER,
+            password: opts.expect_password || WS_PASSWORD,
+            grant_type: 'password'
+        };
+    }
+
+    return (options) => {
+        expect(options).to.eql({
+            baseUrl: WS_URI,
+            method: 'POST',
+            uri: '/oauth2/token',
+            auth: {
+                username: WS_CLIENT_ID,
+                password: WS_CLIENT_SECRET,
+                sendImmediately: true
+            },
+            json: true,
+            body: body
+        });
+
+        if (opts.err) {
+            return Promise.reject(new (opts.err.errorClass || Error)(opts.err.message));
+        } else {
+            return Promise.resolve({
+                access_token: opts.send_access_token || WS_ACCESS_TOKEN,
+                refresh_token: opts.send_refresh_token || WS_REFRESH_TOKEN,
+                expires_in: Date.now() + (opts.expires_in || 10)
+            });
+        }
+    }
+};
+
 /* Exported symbols */
-module.exports = {
+module.exports = Object.freeze({
     StubLogger: StubLogger,
     StubHTTPClient: StubHTTPClient,
-    StubHTTPStatusCodeError: StubHTTPStatusCodeError
-};
+    StubHTTPStatusCodeError: StubHTTPStatusCodeError,
+    getInstance: getInstance,
+    authHandler: authHandler,
+    /* Constants */
+    WS_URI: WS_URI,
+    WS_USER: WS_USER,
+    WS_PASSWORD: WS_PASSWORD,
+    WS_CLIENT_ID: WS_CLIENT_ID,
+    WS_CLIENT_SECRET: WS_CLIENT_SECRET,
+    WS_ACCESS_TOKEN: WS_ACCESS_TOKEN,
+    WS_REFRESH_TOKEN: WS_REFRESH_TOKEN,
+    WS_ACCESS_TOKEN2: WS_ACCESS_TOKEN2,
+    WS_REFRESH_TOKEN2: WS_REFRESH_TOKEN2
+});
