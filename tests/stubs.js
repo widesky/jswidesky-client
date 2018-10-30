abe5bf629f4c8d5801ef9c7d40f94a1d066a65b5
Stuart Longland <stuartl@vrt.com.au>
2018-10-30 10:46:04 +1000

    WSHUB-100: test stubs: Add stubs for HTTP client.

---
+++ tests/stubs.js
@@ -0,0 +1,61 @@
+/*
+ * vim: set tw=78 et ts=4 sw=4 si fileencoding=utf-8:
+ *
+ * Unit test stubs
+ * (C) 2018 VRT Systems
+ */
+"use strict";
+
+/**
+ * StubHTTPStatusCodeError is a class that mimics the `StatusCodeError`
+ * in `request-promise`.
+ */
+const StubHTTPStatusCodeError = function(message) {
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+};
+require('util').inherits(StubHTTPStatusCodeError, Error);
+
+/**
+ * StubHTTPClient is a class that mimics `request-promise`'s interface whilst
+ * providing a means to "spoof" a response as if a real HTTP request had been
+ * made and the response returned.
+ */
+const StubHTTPClient = function() {
+    const self = this;
+    var handler = null;
+
+    /**
+     * Assign a new handler that will receive the request and generate the
+     * expected response.  Handler is expected to take a simple `options`
+     * object in the same manner as `request-promise` and either throw an
+     * exception to terminate the test, or to return the expected data.
+     *
+     * See https://github.com/request/request-promise for an idea of how we
+     * need to behave.  The WideSky client really doesn't use much.
+     *
+     * This may be replaced at any time.
+     */
+    self.setHandler = function(h) {
+        handler = h;
+    };
+
+    /**
+     * Stub a WideSkyClient instance with this HTTP client instance.
+     */
+    self.stubClient = function(ws) {
+        ws._request = (options) => {
+            return handler(options);
+        };
+        ws._rqerr = {
+            StatusCodeError: StubHTTPStatusCodeError
+        };
+    };
+};
+
+/* Exported symbols */
+module.exports = {
+    StubHTTPClient: StubHTTPClient,
+    StubHTTPStatusCodeError: StubHTTPSTatusCodeError
+};
