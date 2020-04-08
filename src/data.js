193b4d98661cc9c6843c084552b56f7e53423f7d
Stuart Longland <stuartl@vrt.com.au>
2020-04-08 18:20:01 +1000

    WC-2803: data: Add parsing and dumping routines.

--- src/data.js
+++ src/data.js
@@ -5,6 +5,9 @@
 
 var jsesc = require('jsesc');
 
+const VER_2 = '2.0',
+    VER_3 = '3.0';
+
 /**
  * Marker data type, a singleton that indicates a tag exists.
  */
@@ -294,8 +297,189 @@ Number.prototype.toHSZINC = function() {
     return this.toHSNumber().toHSZINC();
 };
 
+/**
+ * Parse a JSON string in Haystack ZINC/JSON encoding
+ */
+function _parseString(str) {
+    if (str === 'm:')
+        return MARKER;
+    if (str === 'z:')
+        return NA;
+    if ((str === 'x:') || (str === '-:'))
+        return REMOVE;
+
+    const prefix = str.substring(0, 2);
+
+    if (prefix === 'n:')
+        return Number.fromHS(str);
+    if (prefix === 'r:')
+        return Ref.fromHS(str);
+    if (prefix === 's:')
+        return String.fromHS(str);
+    if (prefix === 't:')
+        return Date.fromHS(str);
+
+    /* Must be a bare string */
+    return str;
+}
+
+/**
+ * Parse a list
+ */
+function _parseList(list) {
+    return list.map((element) => {
+        return parse(element);
+    });
+}
+
+/**
+ * Dump a list
+ */
+function _dumpList(list, version) {
+    return list.map((element) => {
+        return dump(element, version);
+    });
+}
+
+/**
+ * Parse a grid
+ */
+function _parseGrid(grid) {
+    var parsed = {};
+
+    parsed.meta = _parseDict(grid.meta, 'ver');
+    parsed.cols = grid.cols.map((col) => {
+        return _parseDict(col, 'name');
+    });
+    parsed.rows = grid.rows.map((row) => {
+        return _parseDict(row);
+    });
+
+    return parsed;
+}
+
+/**
+ * Dump a grid
+ */
+function _dumpGrid(grid, version) {
+    var dumped = {};
+
+    dumped.meta = _dumpDict(grid.meta, 'ver', version);
+    dumped.cols = grid.cols.map((col) => {
+        return _dumpDict(col, 'name', version);
+    });
+    dumped.rows = grid.rows.map((row) => {
+        return _dumpDict(row, undefined, version);
+    });
+
+    return dumped;
+}
+
+/**
+ * Parse a dict, optionally passing through a property value
+ */
+function _parseDict(dict, passthrough) {
+    var parsed = {};
+
+    if (passthrough) {
+        dict = Object.assign({}, dict);
+        parsed[passthrough] = dict[passthrough];
+        delete dict[passthrough];
+    }
+
+    Object.keys(dict).forEach((tag) => {
+        parsed[tag] = parse(dict[tag]);
+    });
+
+    return parsed;
+};
+
+/**
+ * Dump a dict, optionally passing through a property value
+ */
+function _dumpDict(dict, passthrough, version) {
+    var dumped = {};
+
+    if (passthrough) {
+        dict = Object.assign({}, dict);
+        dumped[passthrough] = dict[passthrough];
+        delete dict[passthrough];
+    }
+
+    Object.keys(dict).forEach((tag) => {
+        dumped[tag] = dump(dict[tag], version);
+    });
+
+    return dumped;
+};
+
+/**
+ * Parse an arbitrary value and return the relevant JavaScript
+ * type.
+ */
+function parse(value) {
+    if ((value === null) || (typeof(value) === 'boolean')) {
+        /* Raw types */
+        return value;
+    } else if (value === undefined) {
+        /* Catch and handle null type */
+        return null;
+    } else if (typeof(value) === 'string') {
+        return _parseString(value);
+    } else if (typeof(value) === 'object') {
+        if (value instanceof Array) {
+            /* Parse the elements */
+            return _parseList(value);
+        } else if (value.hasOwnProperty('meta')
+                    && value.hasOwnProperty('cols')
+                    && value.hasOwnProperty('rows')) {
+            /* This is a grid */
+            return _parseGrid(value);
+        } else {
+            /* This is a dict */
+            return _parseDict(value);
+        }
+    }
+
+    /* If we're still here, then we don't handle it */
+    throw new Error('Unhandled input: ' + JSON.stringify(value));
+};
+
+/**
+ * Dump the given input in JSON format.
+ */
+function dump(value, version) {
+    if ((value === null) || (typeof(value) === 'boolean')) {
+        /* Raw types */
+        return value;
+    } else if (value === undefined) {
+        /* Catch and handle null type */
+        return null;
+    } else if (typeof(value.toHSJSON) === 'function') {
+        return value.toHSJSON(version);
+    } else if (typeof(value) === 'object') {
+        if (value instanceof Array) {
+            /* Dump the elements */
+            return _dumpList(value, version);
+        } else if (value.hasOwnProperty('meta')
+                    && value.hasOwnProperty('cols')
+                    && value.hasOwnProperty('rows')) {
+            /* This is a grid */
+            return _dumpGrid(value, version);
+        } else {
+            /* This is a dict */
+            return _dumpDict(value, undefined, version);
+        }
+    }
+
+    /* If we're still here, then we don't handle it */
+    throw new Error('Unhandled input: ' + JSON.stringify(value));
+}
+
 /* Exported symbols */
 module.exports = {
+    VER_2: VER_2,
+    VER_3: VER_3,
     MARKER: MARKER,
     NA: NA,
     REMOVE: REMOVE,
@@ -303,5 +487,7 @@ module.exports = {
     HSNumber: HSNumber,
     String: String,
     Number: Number,
-    Date: Date
+    Date: Date,
+    parse: parse,
+    dump: dump
 };
