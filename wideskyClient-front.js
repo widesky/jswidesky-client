6a2383f3784a696d8aed5f42e11530d595e96fa5
SamuelToh <samuelt@vrt.com.au>
2020-09-25 15:10:58 +1000

    BA-418: Make jswidesky browser compatible.

---
+++ wideskyClient-front.js
@@ -0,0 +1,30 @@
+/*
+ * vim: set tw=78 et ts=4 sw=4 si fileencoding=utf-8:
+ */
+"use strict";
+
+var client = require('./src/client'),
+    data = require('./src/data');
+
+/* Exported symbols */
+var jsWidesky = {
+    /* Client code */
+    WideSkyClient: client,
+    /* Constants */
+    VER_2: data.VER_2,
+    VER_3: data.VER_3,
+    /* Data types */
+    MARKER: data.MARKER,
+    NA: data.NA,
+    REMOVE: data.REMOVE,
+    Ref: data.Ref,
+    String: data.String,    // polyfilled
+    Number: data.Number,    // polyfilled
+    Date: data.Date,        // polyfilled
+    HSNumber: data.HSNumber,
+    /* Helper routines */
+    parse: data.parse,
+    dump: data.dump
+};
+
+window.jsWidesky = jsWidesky;
