03461177e9f266c5fd5e673f3bf389862d8db2e2
Daniel Henley <daniel.henley@widesky.cloud>
2022-10-31 14:52:56 +1000

    WC-5398: Fixed hisWrite tests

--- test/client/hist.js
+++ test/client/hist.js
@@ -5,19 +5,14 @@
  */
 "use strict";
 
-const WideSkyClient = require('../../src/client'),
-    stubs = require('../stubs'),
+const stubs = require('../stubs'),
     sinon = require('sinon'),
     expect = require('chai').expect,
-    WS_URI = stubs.WS_URI,
     WS_USER = stubs.WS_USER,
     WS_PASSWORD = stubs.WS_PASSWORD,
     WS_CLIENT_ID = stubs.WS_CLIENT_ID,
     WS_CLIENT_SECRET = stubs.WS_CLIENT_SECRET,
     WS_ACCESS_TOKEN = stubs.WS_ACCESS_TOKEN,
-    WS_REFRESH_TOKEN = stubs.WS_REFRESH_TOKEN,
-    WS_ACCESS_TOKEN2 = stubs.WS_ACCESS_TOKEN2,
-    WS_REFRESH_TOKEN2 = stubs.WS_REFRESH_TOKEN2,
     getInstance = stubs.getInstance;
 
 function verifyTokenCall(args) {
@@ -1596,163 +1591,163 @@ describe('client', () => {
     });
 
     describe('hisWrite', () => {
-        it('should generate POST with records in timestamp order', () => {
+        it('should generate POST with records in timestamp order', async () => {
             let http = new stubs.StubHTTPClient(),
                 log = new stubs.StubLogger(),
                 ws = getInstance(http, log);
 
-            /* We expect the following requests */
-            let requestHandlers = [
-                /* First up, an authentication request */
-                stubs.authHandler(),
-                /* The read request is next */
-                (options) => {
-                    expect(options).to.eql({
-                        baseUrl: WS_URI,
-                        gzip: true,
-                        headers: {
-                            Authorization: 'Bearer ' + WS_ACCESS_TOKEN,
-                            Accept: 'application/json'
-                        },
-                        json: true,
-                        method: 'POST',
-                        uri: '/api/hisWrite',
-                        body: {
-                            meta: {ver: "2.0"},
-                            cols: [
-                                {name: "ts"},
-                                {name: "v0", id: "r:my.id"}
-                            ],
-                            rows: [
-                                {
-                                    ts: "t:2016-01-01T00:00Z UTC",
-                                    v0: "n:234.56"
-                                },
-                                {
-                                    ts: "t:2016-01-02T00:00Z UTC",
-                                    v0: "n:123.45"
-                                },
-                                {
-                                    ts: "t:2016-02-01T00:00Z UTC",
-                                    v0: "n:631.42"
-                                }
-                            ]
-                        }
-                    });
-
-                    return Promise.resolve('grid goes here');
+            // Correct spy for function _wsRawSubmit()
+            ws._wsRawSubmit = sinon.stub().callsFake((method, uri, body, config) => {
+                let returnItem;
+                if (uri === "/oauth2/token") {
+                    returnItem = {
+                        access_token: WS_ACCESS_TOKEN
+                    };
+                } else if (uri === "/api/hisWrite") {
+                    returnItem = "grid goes here";
+                } else {
+                    returnItem = {};
                 }
-            ];
 
-            http.setHandler((options) => {
-                expect(requestHandlers).to.not.be.empty;
-                return requestHandlers.shift()(options);
+                return Promise.resolve(returnItem);
             });
 
-            return ws.hisWrite(
+            const result = await ws.hisWrite({
+                't:2016-01-02T00:00Z UTC': {
+                    'r:my.id': 'n:123.45'
+                },
+                't:2016-01-01T00:00Z UTC': {
+                    'r:my.id': 'n:234.56'
+                },
+                't:2016-02-01T00:00Z UTC': {
+                    'r:my.id': 'n:631.42'
+                }
+            });
+            expect(result).to.equal("grid goes here");
+            expect(ws._wsRawSubmit.callCount).to.equal(2);
+            verifyTokenCall(ws._wsRawSubmit.firstCall.args);
+            verifyRequestCall(
+                ws._wsRawSubmit.secondCall.args,
+                "POST",
+                "/api/hisWrite",
                 {
-                    't:2016-01-02T00:00Z UTC': {
-                        'r:my.id': 'n:123.45'
+                    meta: {ver: "2.0"},
+                    cols: [
+                        {name: "ts"},
+                        {name: "v0", id: "r:my.id"}
+                    ],
+                    rows: [
+                        {
+                            ts: "t:2016-01-01T00:00Z UTC",
+                            v0: "n:234.56"
+                        },
+                        {
+                            ts: "t:2016-01-02T00:00Z UTC",
+                            v0: "n:123.45"
+                        },
+                        {
+                            ts: "t:2016-02-01T00:00Z UTC",
+                            v0: "n:631.42"
+                        }
+                    ]
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                        Accept: "application/json"
                     },
-                    't:2016-01-01T00:00Z UTC': {
-                        'r:my.id': 'n:234.56'
-                    },
-                    't:2016-02-01T00:00Z UTC': {
-                        'r:my.id': 'n:631.42'
-                    }
+                    decompress: true
                 }
-            ).then((res) => {
-                expect(res).to.equal('grid goes here');
-            });
+            );
         });
 
-        it('should support multiple points', () => {
+        it('should support multiple points', async () => {
             let http = new stubs.StubHTTPClient(),
                 log = new stubs.StubLogger(),
                 ws = getInstance(http, log);
 
-            /* We expect the following requests */
-            let requestHandlers = [
-                /* First up, an authentication request */
-                stubs.authHandler(),
-                /* The read request is next */
-                (options) => {
-                    expect(options).to.eql({
-                        baseUrl: WS_URI,
-                        gzip: true,
-                        headers: {
-                            Authorization: 'Bearer ' + WS_ACCESS_TOKEN,
-                            Accept: 'application/json'
-                        },
-                        json: true,
-                        method: 'POST',
-                        uri: '/api/hisWrite',
-                        body: {
-                            meta: {ver: "2.0"},
-                            cols: [
-                                {name: "ts"},
-                                {name: "v0", id: "r:my.pt.a"},
-                                {name: "v1", id: "r:my.pt.b"},
-                                {name: "v2", id: "r:my.pt.c"}
-                            ],
-                            rows: [
-                                {
-                                    ts: "t:2016-01-01T00:00Z UTC",
-                                    v0: "n:223466",
-                                    v1: "n:176485"
-                                },
-                                {
-                                    ts: "t:2016-01-02T00:00Z UTC",
-                                    v0: "n:347347",
-                                    v1: "n:123456",
-                                    v2: "n:175332"
-                                },
-                                {
-                                    ts: "t:2016-02-01T00:00Z UTC",
-                                    v0: "n:623672",
-                                    v2: "n:623457"
-                                },
-                                {
-                                    ts: "t:2016-02-02T00:00Z UTC",
-                                    v1: "n:612342",
-                                    v2: "n:873442"
-                                }
-                            ]
-                        }
-                    });
-
-                    return Promise.resolve('grid goes here');
+            // Correct spy for function _wsRawSubmit()
+            ws._wsRawSubmit = sinon.stub().callsFake((method, uri, body, config) => {
+                let returnItem;
+                if (uri === "/oauth2/token") {
+                    returnItem = {
+                        access_token: WS_ACCESS_TOKEN
+                    };
+                } else if (uri === "/api/hisWrite") {
+                    returnItem = "grid goes here";
+                } else {
+                    returnItem = {};
                 }
-            ];
 
-            http.setHandler((options) => {
-                expect(requestHandlers).to.not.be.empty;
-                return requestHandlers.shift()(options);
+                return Promise.resolve(returnItem);
             });
 
-            return ws.hisWrite(
+            const result = await ws.hisWrite({
+                't:2016-01-02T00:00Z UTC': {
+                    'r:my.pt.a': 'n:347347',
+                    'r:my.pt.b': 'n:123456',
+                    'r:my.pt.c': 'n:175332'
+                },
+                't:2016-01-01T00:00Z UTC': {
+                    'r:my.pt.a': 'n:223466',
+                    'r:my.pt.b': 'n:176485'
+                },
+                't:2016-02-01T00:00Z UTC': {
+                    'r:my.pt.a': 'n:623672',
+                    'r:my.pt.c': 'n:623457'
+                },
+                't:2016-02-02T00:00Z UTC': {
+                    'r:my.pt.b': 'n:612342',
+                    'r:my.pt.c': 'n:873442'
+                }
+            });
+            expect(result).to.equal("grid goes here");
+            expect(ws._wsRawSubmit.callCount).to.equal(2);
+            verifyTokenCall(ws._wsRawSubmit.firstCall.args);
+            verifyRequestCall(
+                ws._wsRawSubmit.secondCall.args,
+                "POST",
+                "/api/hisWrite",
                 {
-                    't:2016-01-02T00:00Z UTC': {
-                        'r:my.pt.a': 'n:347347',
-                        'r:my.pt.b': 'n:123456',
-                        'r:my.pt.c': 'n:175332'
+                    meta: {ver: "2.0"},
+                    cols: [
+                        {name: "ts"},
+                        {name: "v0", id: "r:my.pt.a"},
+                        {name: "v1", id: "r:my.pt.b"},
+                        {name: "v2", id: "r:my.pt.c"}
+                    ],
+                    rows: [
+                        {
+                            ts: "t:2016-01-01T00:00Z UTC",
+                            v0: "n:223466",
+                            v1: "n:176485"
+                        },
+                        {
+                            ts: "t:2016-01-02T00:00Z UTC",
+                            v0: "n:347347",
+                            v1: "n:123456",
+                            v2: "n:175332"
+                        },
+                        {
+                            ts: "t:2016-02-01T00:00Z UTC",
+                            v0: "n:623672",
+                            v2: "n:623457"
+                        },
+                        {
+                            ts: "t:2016-02-02T00:00Z UTC",
+                            v1: "n:612342",
+                            v2: "n:873442"
+                        }
+                    ]
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                        Accept: "application/json"
                     },
-                    't:2016-01-01T00:00Z UTC': {
-                        'r:my.pt.a': 'n:223466',
-                        'r:my.pt.b': 'n:176485'
-                    },
-                    't:2016-02-01T00:00Z UTC': {
-                        'r:my.pt.a': 'n:623672',
-                        'r:my.pt.c': 'n:623457'
-                    },
-                    't:2016-02-02T00:00Z UTC': {
-                        'r:my.pt.b': 'n:612342',
-                        'r:my.pt.c': 'n:873442'
-                    }
+                    decompress: true
                 }
-            ).then((res) => {
-                expect(res).to.equal('grid goes here');
-            });
+            );
         });
     });
 });
