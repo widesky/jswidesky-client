fad99ddcb40a47a46b916f76f5bf9f982a9329ac
Daniel Henley <daniel.henley@widesky.cloud>
2022-10-31 15:32:29 +1000

    WC-5398: Refactored functions

--- test/client/hist.js
+++ test/client/hist.js
@@ -8,35 +8,10 @@
 const stubs = require('../stubs'),
     sinon = require('sinon'),
     expect = require('chai').expect,
-    WS_USER = stubs.WS_USER,
-    WS_PASSWORD = stubs.WS_PASSWORD,
-    WS_CLIENT_ID = stubs.WS_CLIENT_ID,
-    WS_CLIENT_SECRET = stubs.WS_CLIENT_SECRET,
     WS_ACCESS_TOKEN = stubs.WS_ACCESS_TOKEN,
     getInstance = stubs.getInstance;
 
-function verifyTokenCall(args) {
-    expect(args[0]).to.equal("POST");
-    expect(args[1]).to.equal("/oauth2/token");
-    expect(args[2]).to.deep.equal({
-        username: WS_USER,
-        password: WS_PASSWORD,
-        grant_type: "password"
-    });
-    expect(args[3]).to.deep.equal({
-        auth: {
-            username: WS_CLIENT_ID,
-            password: WS_CLIENT_SECRET
-        }
-    });
-}
-
-function verifyRequestCall(stubArgs, method, uri, body, config) {
-    expect(stubArgs[0]).to.equal(method);
-    expect(stubArgs[1]).to.equal(uri);
-    expect(stubArgs[2]).to.deep.equal(body);
-    expect(stubArgs[3]).to.deep.equal(config);
-}
+const { verifyTokenCall, verifyRequestCall } = require("./utils");
 
 describe('client', () => {
     describe('hisRead', () => {
---
+++ test/client/utils.js
@@ -0,0 +1,34 @@
+const stubs = require('../stubs'),
+    expect = require('chai').expect,
+    WS_USER = stubs.WS_USER,
+    WS_PASSWORD = stubs.WS_PASSWORD,
+    WS_CLIENT_ID = stubs.WS_CLIENT_ID,
+    WS_CLIENT_SECRET = stubs.WS_CLIENT_SECRET;
+
+function verifyTokenCall(args) {
+    expect(args[0]).to.equal("POST");
+    expect(args[1]).to.equal("/oauth2/token");
+    expect(args[2]).to.deep.equal({
+        username: WS_USER,
+        password: WS_PASSWORD,
+        grant_type: "password"
+    });
+    expect(args[3]).to.deep.equal({
+        auth: {
+            username: WS_CLIENT_ID,
+            password: WS_CLIENT_SECRET
+        }
+    });
+}
+
+function verifyRequestCall(stubArgs, method, uri, body, config) {
+    expect(stubArgs[0]).to.equal(method);
+    expect(stubArgs[1]).to.equal(uri);
+    expect(stubArgs[2]).to.deep.equal(body);
+    expect(stubArgs[3]).to.deep.equal(config);
+}
+
+module.exports = {
+    verifyTokenCall: verifyTokenCall,
+    verifyRequestCall: verifyRequestCall
+}
