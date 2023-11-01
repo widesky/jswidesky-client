2afc6ffa19367d6ecae90e05546002e7ea3a3f1c
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2023-11-01 16:44:04 +1000

    CORE-549: Added better error handling

--- src/client.js
+++ src/client.js
@@ -24,6 +24,7 @@ else {
     // https://github.com/axios/axios/issues/5038#:~:text=Since%20the%20latest,stated%20in%20README
     axios = require('axios').default;
 }
+const { isAxiosError } = axios;
 
 /** Special columns, these will be placed in the given order */
 const SPECIAL_COLS = ['id', 'name', 'dis'];
@@ -217,12 +218,23 @@ class WideSkyClient {
             return await this._wsRawSubmit(method, uri, body, config);
         }
         catch (err) {
-            // If error is 401, then get new token
-            if (err.response && err.response.status === 401 && this._ws_token) {
-                this._ws_token = null;
-                config = await this._attachReqConfig(config);
+            if (isAxiosError(err)) {
+                if (err.response) {
+                    // response has been received from API server
+                    if (err.response.status === 401 && this._ws_token) {
+                        // If error is 401, then get new token
+                        this._ws_token = null;
+                        config = await this._attachReqConfig(config);
 
-                return this._wsRawSubmit(method, uri, body, config);
+                        return this._wsRawSubmit(method, uri, body, config);
+                    }
+                    else {
+                        throw new Error(err.response.data);
+                    }
+                }
+                else {
+                    throw err;
+                }
             }
             else {
                 throw err;
