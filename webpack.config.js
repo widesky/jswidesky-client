1b6560881c8acb1fd9049e0f8d5a758150900b27
Cory Dyer <cory.dyer@widesky.cloud>
2025-05-06 13:58:40 +1000

    CORE-4957: chore: update build to use dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,64 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const webpack = require("webpack");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = (env, argv) => {
-
-    const conf = {
-        target: 'node',
-        mode: argv.mode === 'production' ? 'production' : 'development',
-        devServer: {
-            open: true,
-            openPage: [`client/example.html`],
-            contentBase: path.join(__dirname, '/'),
-            watchContentBase: true,
-            port: 8080,
-            host: argv.mode === 'production' ? `localhost` : `localhost`,
-            disableHostCheck: true,
+module.exports = (env, argv) => ({
+    target: "node",
+    mode: argv.mode === "production" ? "production" : "development",
+    devtool: argv.mode === "production" ? undefined : "inline-source-map",
+    devServer:
+        argv.mode === "production"
+            ? undefined
+            : {
+                  open: true,
+                  openPage: [`client/example.html`],
+                  contentBase: path.join(__dirname, "/"),
+                  watchContentBase: true,
+                  port: 8080,
+                  host: "localhost",
+                  disableHostCheck: true,
+              },
+    entry: {
+        index: [`./index.js`],
+    },
+    output: {
+        library: {
+            type: "umd",
+            name: "JsWideSky",
+            umdNamedDefine: true,
         },
-        entry: {
-           'jsWideSky': [`./index.js`],
-        },
-        // library building properties for (1-1)
-        output: {
-            path: path.join(__dirname, '/dist/'),
-            filename: argv.mode === 'production' ? `[name].min.js` : `[name].develop.js`,
-            library: {
-                type: 'umd',
-                name: 'JsWideSky',
-                umdNamedDefine: true
-            },
-            globalObject: 'this',
-        },
-        optimization: {
-            minimizer: [new TerserPlugin({
+        globalObject: "this",
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                "LICENSE",
+                "package.json",
+                "CHANGELOG.md",
+                "README.md",
+                { from: "docs/client", to: "docs" },
+            ],
+        }),
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
                 terserOptions: {
                     compress: {
                         drop_console: true,
                     },
                     output: {
-                        ascii_only: true
-                    }
-                }
-            })],
+                        ascii_only: true,
+                    },
+                },
+            }),
+        ],
+    },
+    resolve: {
+        fallback: {
+            bufferutil: false,
+            "utf-8-validate": false,
         },
-        module: {
-            rules: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [{
-                    loader: 'babel-loader',
-                }]
-            }]
-        },
-        resolve: {
-            fallback: {
-                fs: false,
-                net: false,
-                tls: false,
-                url: false,
-                http: false,
-                stream: false,
-                assert: false,
-                zlib: false,
-                buffer: false,
-                https: false,
-                path: false,
-                express: false,
-                bufferutil: false,
-                'utf-8-validate': false,
-            }
-        },
-        externals: [
-            "dtrace-provider",
-            'fs',
-            'mv',
-            'os',
-            'source-map-support'
-        ]
-    };
-
-    if (argv.mode !== 'production') {
-        conf.devtool = 'inline-source-map';
-    }
-
-    return conf;
-
-};
+    },
+    externals: ["dtrace-provider"],
+});
