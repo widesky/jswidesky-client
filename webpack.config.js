c58329cec728a28acb3cefc1776dd0602393914e
Cory Dyer <cory.dyer@widesky.cloud>
2025-05-26 19:00:49 +1000

    CORE-5080: chore: remove verbose arguments and file names for build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
-const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => ({
     mode: argv.mode,
     devtool: argv.mode === "production" ? undefined : "inline-source-map",
     entry: {
-        jsWideSky: [`./index.js`],
+        index: [`./index.js`],
     },
-    // library building properties for (1-1)
     output: {
-        path: path.join(__dirname, "/dist/"),
-        filename:
-            argv.mode === "production" ? `[name].min.js` : `[name].develop.js`,
         library: {
             type: "umd",
             name: "JsWideSky",
