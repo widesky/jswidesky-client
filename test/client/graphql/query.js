bb43102a97b25d7b8afe0829ee84e06186191409
Cory Dyer <cory.dyer@widesky.cloud>
2025-11-24 12:01:31 +1000

    CORE-7115: tests: add tests for new metadata parameter on `query` function

--- test/client/graphql/query.js
+++ test/client/graphql/query.js
@@ -64,5 +64,169 @@ describe('client', () => {
                 }
             );
         });
+
+        describe('metadata', () => {
+            it('should handle undefined metadata', async () => {
+                await ws.query('graphql query here', undefined);
+
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+
+            it('should handle stringified metadata', async () => {
+                const metadata = JSON.stringify({
+                    testOne: 'simple string',
+                    testTwo: { innerObject: 'inner string' },
+                    testThree: ['value1', 'value2'],
+                });
+                await ws.query('graphql query here', metadata);
+
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                        metadata: {
+                            testOne: 'simple string',
+                            testThree: ['value1', 'value2'],
+                            testTwo: {
+                                innerObject: 'inner string',
+                            },
+                        },
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+
+            it('should handle object metadata', async () => {
+                const metadata = {
+                    testOne: 'simple string',
+                    testTwo: { innerObject: 'inner string' },
+                    testThree: ['value1', 'value2'],
+                };
+                await ws.query('graphql query here', metadata);
+
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                        metadata: {
+                            testOne: 'simple string',
+                            testThree: ['value1', 'value2'],
+                            testTwo: {
+                                innerObject: 'inner string',
+                            },
+                        },
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+
+            it('should handle malformed metadata (number)', async () => {
+                const metadata = 200;
+                await ws.query('graphql query here', metadata);
+
+                expect(log.warn.callCount).to.equal(1);
+                expect(log.warn.firstCall.args[0]).to.equal('Metadata failed to parse:');
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                        // No metadata
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+
+            it('should handle malformed metadata (boolean)', async () => {
+                const metadata = true;
+                await ws.query('graphql query here', metadata);
+
+                expect(log.warn.callCount).to.equal(1);
+                expect(log.warn.firstCall.args[0]).to.equal('Metadata failed to parse:');
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                        // No metadata
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+
+            it('should handle malformed metadata (malformed string)', async () => {
+                const metadata = '{ badJSONObject: { ';
+                await ws.query('graphql query here', metadata);
+
+                expect(log.warn.callCount).to.equal(1);
+                expect(log.warn.firstCall.args[0]).to.equal('Metadata failed to parse:');
+                expect(ws._wsRawSubmit.callCount).to.equal(2);
+                verifyRequestCall(
+                    ws._wsRawSubmit.secondCall.args,
+                    'POST',
+                    '/graphql',
+                    {
+                        query: '{ graphql query here }',
+                        // No metadata
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${WS_ACCESS_TOKEN}`,
+                            Accept: 'application/json',
+                        },
+                        decompress: true,
+                    }
+                );
+            });
+        });
     });
 });
